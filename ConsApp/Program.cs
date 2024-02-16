e8e8993f9480cd46543fd750a2f809a802e75c21
shreya <shreyachinmayi@gmail.com>
2024-02-16 09:35:23 +0530

    Add project files.

---
+++ ConsApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsApp;
+using System.Globalization;
+Console.WriteLine("WELCOME TO NUMBERSYSTEM");
+Console.WriteLine("**Even Numbers**");
+NumberSystem.EvenNum();
+Console.WriteLine("**Odd Numbers* *");
+NumberSystem.OddNum();
+
